d3119e71c1572039ed334a62a574428df8d304da
Christoph Raupach <craupach@gmail.com>
2008-12-18 14:30:41 +0000

    Initial import. Keep fingers crossed

---
+++ src/RobotSwarmSimulator.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : RobotSwarmSimulator.cpp
+// Author      : Christoph Raupach
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello swarm!" << endl; // prints Hello swarm!
+	return 0;
+}
