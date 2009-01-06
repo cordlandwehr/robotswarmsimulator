8b5aedb3c878dc16db17a8f61862582164a22461
Martina Hüllmann <martinah@uni-paderborn.de>
2009-01-06 17:55:02 +0000

    Added access-methods for the already defined class members of class WorldInformation.

--- src/Model/world_information.cc
+++ src/Model/world_information.cc
@@ -7,3 +7,27 @@ WorldInformation::WorldInformation() {
 WorldInformation::~WorldInformation() {
 	// TODO Auto-generated destructor stub
 }
+
+const vector<boost::shared_ptr<WorldObject> >& WorldInformation::markers() const {
+	return markers_;
+}
+
+void WorldInformation::add_marker(boost::shared_ptr<WorldObject> new_marker) {
+	markers_.push_back(new_marker);
+}
+
+const vector<boost::shared_ptr<Obstacle> >& WorldInformation::obstacles() const {
+	return obstacles_;
+}
+
+void WorldInformation::add_obstacle(boost::shared_ptr<Obstacle> new_obstacle) {
+	obstacles_.push_back(new_obstacle);
+}
+
+const vector<boost::shared_ptr<Robot> >& WorldInformation::robots() const {
+	return robots_;
+}
+
+void WorldInformation::add_robot(boost::shared_ptr<Robot> new_robot) {
+	robots_.push_back(new_robot);
+}
--- src/Model/world_information.h
+++ src/Model/world_information.h
@@ -1,4 +1,5 @@
 /**
+ * \class WorldInformation
  * \brief	Each WorldInformation instance corresponds
  * 			to the state of the simulated world at a
  * 			specific simulation step.
@@ -9,20 +10,72 @@
 #include <vector>
 #include <boost/smart_ptr.hpp>
 
-#include "world_object.h"
-#include "robot.h"
-#include "obstacle.h"
+using namespace std;
+
+class WorldObject;
+class Robot;
+class Obstacle;
 
 class WorldInformation {
+
 public:
 	WorldInformation();
 	virtual ~WorldInformation();
 
+	/**
+	 * Returns a constant reference to the set of the markers.
+	 * \return Constant reference to the set of the markers.
+	 */
+	const vector<boost::shared_ptr<WorldObject> >& markers() const;
+
+	//TODO (martinah) maybe set_marker() instead of (or additionally?) this method
+	/**
+	 * Adds a new marker to the world.
+	 * \param a shared pointer to the new marker
+	 */
+	void add_marker(boost::shared_ptr<WorldObject> new_marker);
+
+	/**
+	 * Returns a constant reference to the set of the obstacles.
+	 * \return Constant reference to the set of the obstacles.
+	 */
+	const vector<boost::shared_ptr<Obstacle> >& obstacles() const;
+
+	//TODO (martinah) maybe set_obstacle() instead of (or additionally?) this method
+	/**
+	 * Adds a new obstacle to the world.
+	 * \param a shared pointer to the new obstacle
+	 */
+	void add_obstacle(boost::shared_ptr<Obstacle> new_obstacle);
+
+	/**
+	 * Returns a constant reference to the set of the robots.
+	 * \return Constant reference to the set of the robots.
+	 */
+	const vector<boost::shared_ptr<Robot> >& robots() const;
+
+	//TODO (martinah) maybe set_robot() instead of (or additionally?) this method
+	/**
+	 * Adds a new obstacle to the world.
+	 * \param a shared pointer to the new robot
+	 */
+	void add_robot(boost::shared_ptr<Robot> new_robot);
+
 private:
-	//TODO: boost::ptr_vector<>? (more efficient, but slightly different behavior)
-	std::vector< boost::shared_ptr<WorldObject> > markers;
-	std::vector< boost::shared_ptr<Obstacle> > obstacles;
-	std::vector< boost::shared_ptr<Robot> > robots;
+	/**
+	 * Set of markers in the world
+	 */
+	std::vector< boost::shared_ptr<WorldObject> > markers_;
+
+	/**
+	* Set of obstacles in the world
+	*/
+	std::vector< boost::shared_ptr<Obstacle> > obstacles_;
+
+	/**
+	* Set of robots in the world
+	*/
+	std::vector< boost::shared_ptr<Robot> > robots_;
 };
 
 #endif /* WORLD_INFORMATION_H_ */
