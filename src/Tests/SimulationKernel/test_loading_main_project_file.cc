8cbbcb42c54dd4e01319fbd8bf2bd1e82786c1db
Martina Hüllmann <martinah@uni-paderborn.de>
2009-01-11 22:57:45 +0000

    Adapted Loading-Main-Project-File-UnitTest to my previous modifications.

--- src/Tests/SimulationKernel/test_loading_main_project_file.cc
+++ src/Tests/SimulationKernel/test_loading_main_project_file.cc
@@ -6,8 +6,11 @@ BOOST_AUTO_TEST_CASE(loading_main_project_file)
 {
 	//create simulation kernel
 	SimulationKernel* sim = new SimulationKernel();
-	sim->init("/home/martinah/Studium/PG/SVN/der-schwarm/codebase/RobotSwarmSimulator/src/ProjectFiles/projectfile.swarm");
 
+	//initialize simulation kernel
+	sim->init("src/ProjectFiles/projectfile.swarm");
+
+	//check variables read from main project file
 	BOOST_CHECK_EQUAL(sim->project_name(), "My Exciting Project");
 	BOOST_CHECK_EQUAL(sim->compass_model(), "NO_COMPASS");
 	BOOST_CHECK_EQUAL(sim->robot_filename(), "myrobots");
@@ -15,6 +18,4 @@ BOOST_AUTO_TEST_CASE(loading_main_project_file)
 	BOOST_CHECK_EQUAL(sim->statistics_module(), 0);
 	BOOST_CHECK_EQUAL(sim->asg(), 0);
 	BOOST_CHECK_EQUAL(sim->event_handler(), 0);
-	BOOST_CHECK_EQUAL(sim->algorithm(),"CLEVER_ALGO");
-
 }
