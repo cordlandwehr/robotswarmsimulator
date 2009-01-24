8c0b976358d40db29cdb5926f68ae4a127b2ffd9
Martina Hüllmann <martinah@uni-paderborn.de>
2009-01-24 18:30:13 +0000

    Added Parser-Class (Class not finished yet, I think I'll finish implementing loading methods this weekend). Moved and modified old Parser-Tests.

---
+++ src/Tests/Utilities/parser_test.cc
@@ -0,0 +1,57 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/smart_ptr.hpp>
+
+#include "../../Utilities/parser.h"
+#include "../../SimulationKernel/simulation_kernel.h"
+#include "../../SimulationControl/history.h"
+
+//
+// INFO
+// The parser doesn't handle wrong inputs now.
+// Will be fixed soon.
+//
+
+BOOST_AUTO_TEST_CASE(loading_main_project_file_1)
+{
+	Parser* parser = new Parser();
+	parser->load_projectfiles("src/ProjectFiles/projectfile.swarm");
+
+	//check variables read from main project file
+	BOOST_CHECK_EQUAL(parser->project_name_, "My Exciting Project");
+	BOOST_CHECK_EQUAL(parser->compass_model_, "NO_COMPASS");
+	BOOST_CHECK_EQUAL(parser->robot_filename_, "myrobots");
+	BOOST_CHECK_EQUAL(parser->obstacle_filename_, "myobstacles");
+	BOOST_CHECK_EQUAL(parser->statistics_module_, 0);
+	BOOST_CHECK_EQUAL(parser->asg_, 0);
+	BOOST_CHECK_EQUAL(parser->event_handler_, 0);
+}
+
+BOOST_AUTO_TEST_CASE(loading_main_project_file_2)
+{
+	Parser* parser = new Parser();
+	parser->load_projectfiles("src/Tests/Testdata/garbled_projectfile_a.swarm");
+
+	//check variables read from main project file
+	BOOST_CHECK_EQUAL(parser->project_name_, "My Exciting Project");
+	BOOST_CHECK_EQUAL(parser->compass_model_, "NO_COMPASS");
+	BOOST_CHECK_EQUAL(parser->robot_filename_, "myrobots");
+	BOOST_CHECK_EQUAL(parser->obstacle_filename_, "myobstacles");
+	BOOST_CHECK_EQUAL(parser->statistics_module_, 0);
+	BOOST_CHECK_EQUAL(parser->asg_, 0);
+	BOOST_CHECK_EQUAL(parser->event_handler_, 0);
+}
+
+BOOST_AUTO_TEST_CASE(loading_main_project_file_3)
+{
+	Parser* parser = new Parser();
+	parser->load_projectfiles("src/Tests/Testdata/garbled_projectfile_b.swarm");
+
+	//check variables read from main project file
+	BOOST_CHECK_EQUAL(parser->project_name_, "My Exciting Project");
+	BOOST_CHECK_EQUAL(parser->compass_model_, "NO_COMPASS");
+	BOOST_CHECK_EQUAL(parser->robot_filename_, "myrobots");
+	BOOST_CHECK_EQUAL(parser->obstacle_filename_, "myobstacles");
+	BOOST_CHECK_EQUAL(parser->statistics_module_, 0);
+	BOOST_CHECK_EQUAL(parser->asg_, 0);
+	BOOST_CHECK_EQUAL(parser->event_handler_, 0);
+}
