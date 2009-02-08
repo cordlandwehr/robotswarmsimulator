df303ffc138702f333c48caa9133811cb8441709
Martina Hüllmann <martinah@uni-paderborn.de>
2009-02-08 20:35:52 +0000

    Added hash-map (and methods for inserting and getting data from it) containing data of a marker information object.

--- src/Model/marker_information.cc
+++ src/Model/marker_information.cc
@@ -1,4 +1,5 @@
 #include "marker_information.h"
+#include "../Utilities/unsupported_operation_exception.h"
 
 MarkerInformation::MarkerInformation() {
 	// TODO Auto-generated constructor stub
@@ -11,4 +12,22 @@ MarkerInformation::~MarkerInformation() {
 
 boost::shared_ptr<MarkerInformation> MarkerInformation::clone() const {
 	return boost::shared_ptr<MarkerInformation>(new MarkerInformation(*this));
+}
+
+void MarkerInformation::add_data(string var_name, boost::any insert_data) {
+	data.insert(pair<string,boost::any>(var_name,insert_data));
+}
+
+boost::any MarkerInformation::get_data(string var_name) {
+	//get iterator to value corresponding to key var_name
+	map<string,boost::any>::const_iterator map_iterator = data.find(var_name);
+
+	//check if var_name exists in given map
+	if(map_iterator == data.end()) {
+		//var_name doesn't exist in given map
+		throw UnsupportedOperationException("Variable "+var_name+" doesn't hold any data.");
+	} else {
+		//return value of var_name
+		return data.find(var_name)->second;
+	}
 }
--- src/Model/marker_information.h
+++ src/Model/marker_information.h
@@ -6,23 +6,48 @@
 #define MARKER_INFORMATION_H_
 
 #include <boost/smart_ptr.hpp>
+#include <boost/any.hpp>
+#include <string>
+#include <map>
+
+using namespace std;
 
 // TODO(peter) comment class
-// TODO(peter) redesign class to met the following design (eMail discussion Christoph/Martina/Peter)
-//             - it is NOT designed as a base class, so do not use any virtual methods
-//             - carried data will be placed inside a map using a string ('variable name') as key and boost::any as
-//               value for the key (boost::any holding the custom data)
 class MarkerInformation {
 public:
 	MarkerInformation();
-	virtual ~MarkerInformation();
+
+	//TODO(martinah) Is this method supposed to be virtual?
+	~MarkerInformation();
 
 	/**
 	 * Clones this object and returns a shared ptr to the cloned object.
 	 * typeid(*this) == typeid(*clone)
 	 * @return shared ptr to the cloned object
 	 */
-	virtual boost::shared_ptr<MarkerInformation> clone() const;
+	boost::shared_ptr<MarkerInformation> clone() const;
+
+	/**
+	 * This method adds data to current marker.
+	 * \param var_name		Name of variable (used as key)
+	 * \param insert_data	Data to be stored.
+	 */
+	void add_data(string var_name, boost::any insert_data);
+
+	/**
+	 * This method returns data stored in given variable.
+	 * \param 	var_name	Return data of this variable.
+	 * \return	Data stored in data-map for given variable.
+	 */
+	boost::any get_data(string var_name);
+
+private:
+	/**
+	 * Contains data of this marker. A string (that denotes 'variable name') is used as a key
+	 * and a boost::any as a value for the key (holds custom data)
+	 * TODO(martinah) Use boost::shared_ptr<boost::any> instead of just boost::any?
+	 */
+	map<string, boost::any> data;
 };
 
 #endif /* MARKER_INFORMATION_H_ */
