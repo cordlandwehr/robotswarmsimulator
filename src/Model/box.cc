9875a3dbfea449faf072784ceba94df9785a62c2
Martina Hüllmann <martinah@uni-paderborn.de>
2009-02-09 13:21:19 +0000

    Implemented point_contained_in-methods for boxes and spheres.

--- src/Model/box.cc
+++ src/Model/box.cc
@@ -54,12 +54,10 @@ bool Box::contains_point(boost::shared_ptr<Vector3d> point) const {
 	//this.y - height/2 <= point.y <= this.y + height/2 AND
 	//this.z - depth/2 <= point.z <= this.z + depth/2
 	//remember: position is the center point of the box
-	/*
-	 * TODO implement using type Vector3d
-	if( position_[0] - width_/2. <= point[0] && point[0] <= position_[0]+width_/2. &&
-	    position_[1] - height_/2. <= point[1] && point[1] <= position_[0]+height_/2. &&
-	    position_[2] - depth_/2.<= point[2] && point[2] <= position_[2]+depth_/2. )
+	if( (*position_)(0) - width_/2. <= (*point)(0) && (*point)(0) <= (*position_)(0)+width_/2. &&
+	    (*position_)(1) - height_/2. <= (*point)(1) && (*point)(1) <= (*position_)(1)+height_/2. &&
+	    (*position_)(2) - depth_/2.<= (*point)(2) && (*point)(2) <= (*position_)(2)+depth_/2. )
 		return true;
-	else*/
+	else
 		return false;
 }
--- src/Model/sphere.cc
+++ src/Model/sphere.cc
@@ -35,13 +35,11 @@ void Sphere::set_radius(double new_radius) {
 bool Sphere::contains_point(boost::shared_ptr<Vector3d> point) const {
 	//Point is contained if
 	//(point.x - position.x)^2 + (point.y - position.y)^2 + (point.z - position.z)^2 <= r^2
-	/*
-	 * TODO implement using type Vector3d
-	if( pow(point[0]-position_[0], 2) +
-	    pow(point[1]-position_[1], 2) +
-	    pow(point[2]-position_[2], 2) <= pow(radius_, 2) )
+	if( pow((*point)(0)-(*position_)(0), 2) +
+	    pow((*point)(1)-(*position_)(1), 2) +
+	    pow((*point)(2)-(*position_)(2), 2) <= pow(radius_, 2) )
 		return true;
-	else*/
+	else
 		return false;
 }
 
