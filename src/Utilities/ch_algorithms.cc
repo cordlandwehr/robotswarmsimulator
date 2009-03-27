6e2c888bf1957197f0aa7a450f1e6d50dc9d9fa6
Martina Hüllmann <martinah@uni-paderborn.de>
2009-03-27 22:46:45 +0000

    Added (empty) class for CH algorithms. Filling this class with code, i.e. implementation of the algorithms required, will follow after my CCT-exam...

---
+++ src/Utilities/ch_algorithms.cc
@@ -0,0 +1,17 @@
+/*
+ * ch_algorithms.cc
+ *
+ *  Created on: 27.03.2009
+ *      Author: martinah
+ */
+
+#include <Utilities/ch_algorithms.h>
+
+CHAlgorithms::CHAlgorithms() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CHAlgorithms::~CHAlgorithms() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/Utilities/ch_algorithms.h
@@ -0,0 +1,26 @@
+/*
+ * ch_algorithms.h
+ *
+ *  Created on: 27.03.2009
+ *      Author: martinah
+ *
+
+
+/**
+ * \class CHAlgorithms
+ *
+ * This class will provide some useful algorithms working with a convex hull.
+ *
+ */
+
+#ifndef CH_ALGORITHMS_H_
+#define CH_ALGORITHMS_H_
+
+class CHAlgorithms {
+public:
+	CHAlgorithms();
+	virtual ~CHAlgorithms();
+
+};
+
+#endif /* CH_ALGORITHMS_H_ */
