702026f305460d79982ad44a298fec63de708cdd
Peter Kling <kronos@uni-paderborn.de>
2009-04-01 15:32:55 +0000

    Completed simple collision handler for position requests. Added example project files to test the two different strategies (STOP and TOUCH). STOP: On detecting a possible collision for the current request, the request will be discarded. TOUCH: On detecting a possible collision, the robot is moved back towards it's original position until it is collision-free.

--- src/EventHandlers/collision_position_request_handler.cc
+++ src/EventHandlers/collision_position_request_handler.cc
@@ -30,31 +30,82 @@ void CollisionPositionRequestHandler::handle_request_reliable(boost::shared_ptr<
 	
 	// get requesting robot
 	const boost::shared_ptr<RobotIdentifier>& robot_id = position_request->robot().id();
-	RobotData& robot_data = world_information->get_according_robot_data(robot_id);
+	RobotData& robot = world_information->get_according_robot_data(robot_id);
+	
+	// if robot is already collided, we abort (no good way to resolve such collisions)
+	if (find_colliding_robot(robot, world_information->robot_data()))
+		return;
 	
 	// handle request, but save robot's current position
-	const Vector3d old_robot_position = robot_data.position();
+	const Vector3d old_position = robot.position();
 	VectorRequestHandler::handle_request_reliable(world_information, request); // handling itself is done by super class
 	
+	// collision handling
+	if (strategy_ == STOP) {
+		// STOP strategy: reject request on collision (i.e. undo the already applied request)
+		if (find_colliding_robot(robot, world_information->robot_data())) {
+			boost::shared_ptr<Vector3d> old_position_ptr(new Vector3d(old_position));
+			robot.set_position(old_position_ptr);
+		}
+	} else if (strategy_ == TOUCH) {
+		// TOUCH strategy: on collision, move back until colliding robots are touching
+		while (const RobotPtr& other_robot = find_colliding_robot(robot, world_information->robot_data())) {
+			move_back_to_touchpoint(robot, *other_robot, old_position);
+		}
+	}
+}
+
+
+// This method essentially solves the quadratic equation for the value alpha that indicates how far the given robot must
+// be moved backwards towards it's old_position such that it has distance clearance_ to the other_robot.
+// Note that the resulting alpha should always lie in the closed interval [0,1].
+void CollisionPositionRequestHandler::move_back_to_touchpoint(RobotData& robot, const RobotData& other_robot,
+                                                              const Vector3d& old_position) {
+	using namespace boost::numeric::ublas;
+	
+	// some values needed for the computation
+	const Vector3d new_to_old          = old_position - robot.position();
+	const Vector3d new_to_other        = other_robot.position() - robot.position();
+	const double vector_product        = inner_prod(new_to_old, new_to_other);
+	const double new_to_old_dist       = norm_2(new_to_old);
+	const double new_to_other_dist     = norm_2(new_to_other);
+	const double new_to_old_dist_sqr   = new_to_old_dist*new_to_old_dist;
+	const double new_to_other_dist_sqr = new_to_other_dist*new_to_other_dist;
+	
+	// The result of the general solution term for quadratic equations (splitted in the two summands; note that we need
+	// only the 'quadr_sol_left + quadr_sol_right' solution and not the 'quadr_sol_left - quadr_sol_right' (which is
+	// negative).
+	const double quadr_sol_left = vector_product/new_to_old_dist_sqr;
+	const double quadr_sol_right = std::sqrt(  vector_product*vector_product
+	                                         + clearance_*clearance_ * new_to_old_dist_sqr
+	                                         - new_to_other_dist_sqr * new_to_old_dist_sqr) / new_to_old_dist_sqr;
+	const double quadr_solution = quadr_sol_left + quadr_sol_right;
+	
+	// Due to rounding errors, we have to be somewhat tollerant. More exactly:
+	//     - make sure that the solution is <= 1 (if violated, then due to rounding errors!)
+	//     - we allow/force an (very small) epsilon gap between two objects, to make sure that these objects are not
+	//       considered as colliding again (would cause a livelock)
+	const double epsilon = 1E-12; // force a gap of (maximum) size 10^(-12) between the two objects
+	const double alpha = std::min(1., quadr_solution + epsilon/new_to_old_dist);
+	
+	// place robot at corresponding position
+	boost::shared_ptr<Vector3d> corrected_position(new Vector3d(robot.position()+alpha*new_to_old));
+	robot.set_position(corrected_position);
+}
+
+
+CollisionPositionRequestHandler::RobotPtr CollisionPositionRequestHandler::find_colliding_robot(const RobotData& robot, const RobotContainer& robots) {
 	// TODO(peter): Collisions are currently detected by iterating over all robots in the world. It would be much more
 	//    efficient (at least for many robots) if we find colliding robots using a dedicated data structure as for
 	//    example an octree. However, we have to be able to change the octree if we change the robot's position (e.g.
 	//    remove the object from it's old position in the octree and readd it using it's new position). As far as I've
 	//    seen, our Octree class does not yet support such methods.
-	typedef boost::shared_ptr<const RobotData> ConstRobotPtr;
-	BOOST_FOREACH(const ConstRobotPtr& other_robot_data, world_information->robot_data()) {
-		if (other_robot_data.get() == &robot_data) // skip 'self-collision'
-			continue;
-		
-		double distance = boost::numeric::ublas::norm_2(robot_data.position() - other_robot_data->position());
-		if (distance < clearance_) { // collision detected
-			if (strategy_ == STOP) {
-				// undo the (already applied) request
-				boost::shared_ptr<Vector3d> old_position_ptr(new Vector3d(old_robot_position));
-				robot_data.set_position(old_position_ptr);
-			} else if (strategy_ == TOUCH) {
-				// TODO(peter): implement
-			}
-		}
+	BOOST_FOREACH(const RobotPtr& other_robot, robots) {
+		if (other_robot.get() == &robot)
+			continue; // skip 'self-collision'
+		const double distance = boost::numeric::ublas::norm_2(other_robot->position() - robot.position());
+		if (distance < clearance_)
+			return other_robot; // found collision
 	}
+	return RobotPtr(); // no collision found
 }
--- src/EventHandlers/collision_position_request_handler.h
+++ src/EventHandlers/collision_position_request_handler.h
@@ -26,6 +26,9 @@
  */
 class CollisionPositionRequestHandler : public VectorRequestHandler {
 public:
+	typedef boost::shared_ptr<RobotData> RobotPtr;
+	typedef std::vector<RobotPtr> RobotContainer;
+	
 	enum CollisionStrategy {
 		STOP, ///< If a collision is detected, the request is discarded.
 		TOUCH ///< If a collision is detected, the robot is moved as far as possible in the requested direction.
@@ -47,10 +50,32 @@ protected:
 	                                     boost::shared_ptr<const Request> request);
 										 
 	
-private:
+private:	
+	/**
+	 * Checks wether one of the robots in the given container is colliding with the given robot.
+	 *
+	 * If such a robot is found, a pointer to it is returned. If not, a NULL pointer is returned. Note that the
+	 * container may contain the regarded robot itself; however, 'self-collision' is not considered, that is, this will
+	 * never return a pointer to the robot itself.
+	 */
+	RobotPtr find_colliding_robot(const RobotData& robot, const RobotContainer& robots);
+	
+	
+	/**
+	 * Moves the given robot back on the line between it's current position and the given old_position such that is no
+	 * longer colliding but touching the given other robot. It is assumed that old_position is valid, i.e. collision
+	 * free (the robot will <em>not</em> be moved farther back than this point).
+	 *
+	 * \param robot Robot that is to be moved back.
+	 * \param other_robot Collision partner the robot is colliding with.
+	 * \param old_position Original position of the robot (that is before the collision occured; this is assumed to be
+	 *        collision free.
+	 */
+	void move_back_to_touchpoint(RobotData& robot, const RobotData& other_robot, const Vector3d& old_position);
+	
+	
 	const CollisionStrategy strategy_; ///< Indicates the strategy used if a collision is detected.
 	const double clearance_; ///< Two robots having a distance less than this value are considered collided.
 };
-
 
 #endif /* COLLISION_POSITION_REQUEST_HANDLER_H */
