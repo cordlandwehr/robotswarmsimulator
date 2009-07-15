e93bca4f25c942d9fe71bb3026dab0564700aaf7
Daniel Wonisch <dwonisch@campus.uni-paderborn.de>
2009-07-15 22:47:03 +0000

    Finished lattice "flow" algorithm (seems to work).

--- src/Playground/dwonisch/local_lattice_flow.lua
+++ src/Playground/dwonisch/local_lattice_flow.lua
@@ -1,7 +1,7 @@
 
 --lua-function for calculating distances
 function dist(vector)
-	local distance = math.sqrt(vector.x^2 + vector.y^2 + vector.z^2);
+	local distance = math.sqrt(vector.x^2 + vector.y^2 + vector.z^2);	
 	return distance;
 end
 
@@ -23,20 +23,25 @@ end
 function find_lowest_positions(robots, x_dir, y_dir) 
 
 	local self_count = countRobotsOnPos(robots, Vector3d(0,0,0));
-	local left_count = countRobotsOnPos(robots, -x_dir);
+	local left_count = countRobotsOnPos(robots, (-1) * x_dir);
 	local right_count = countRobotsOnPos(robots, x_dir);
 	local top_count = countRobotsOnPos(robots, y_dir);
-	local bottom_count = countRobotsOnPos(robots, -y_dir);
+	local bottom_count = countRobotsOnPos(robots, (-1) * y_dir);
 	
-	local min_count = Math.min(self_count, left_count, right_count, top_count, bottom_count);
+	local min_count = math.min(self_count, left_count, right_count, top_count, bottom_count);
 
 	local local_positions = {};
 	
 	if(self_count == min_count) then
 		local_positions[#local_positions + 1] = Vector3d(0,0,0);
+		if(self_count == 0) then
+			--dont move when already at free spot
+			add_color_change_request(1)
+			return local_positions;
+		end			
 	end
 	if(left_count == min_count) then
-		local_positions[#local_positions + 1] = -x_dir;
+		local_positions[#local_positions + 1] = (-1) * x_dir;
 	end
 	if(right_count == min_count) then
 		local_positions[#local_positions + 1] = x_dir;
@@ -45,8 +50,10 @@ function find_lowest_positions(robots, x_dir, y_dir)
 		local_positions[#local_positions + 1] = y_dir;
 	end
 	if(bottom_count == min_count) then
-		local_positions[#local_positions + 1] = -y_dir;
+		local_positions[#local_positions + 1] = (-1) * y_dir;
 	end
+	
+	add_color_change_request(4);
 	
 	return local_positions;	
 end
@@ -54,7 +61,7 @@ end
 function countRobotsOnPos(robots, vector) 
 	local count = 0;
 	for i =1, #robots do 
-		if(dist(robots[i] - vector) < EPS) then
+		if(dist(get_position(robots[i]) - vector) < EPS) then
 			count = count + 1;
 		end
 	end
@@ -64,7 +71,7 @@ end
 
 -- returns x_dir and y_dir as defined at find_lowest_position, if possible; nil otherwise
 function find_lattice_axis(robots) 
-	for i =1, #robots do 
+	for i =1, #robots do 		
 		if(dist(get_position(robots[i])) > EPS) then
 			local x_dir = get_position(robots[i]);
 			return x_dir, get_normal(x_dir);
@@ -84,7 +91,7 @@ function main()
 		add_position_request(Vector3d(0,1.0,0));
 	else
 		local lowest_positions = find_lowest_positions(robots, x_dir, y_dir);
-		local random_index = Math.random(1, #lowest_positions);
-		add_position_request(lowest_positions[random_index]);
+		local random_index = math.random(1, #lowest_positions);
+		add_position_request(lowest_positions[random_index]);		
 	end
 end
