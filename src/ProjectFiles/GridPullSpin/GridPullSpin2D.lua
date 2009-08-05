5f1ccfa55f111ac96a9fedae553af2bef4e87ada
Peter Kling <kronos@uni-paderborn.de>
2009-08-05 16:07:37 +0000

    Some changes to the GridPullSpin algorithm (more like the version used in the proof). Added some new robot arrangements.

--- src/ProjectFiles/GridPullSpin/GridPullSpin2D.lua
+++ src/ProjectFiles/GridPullSpin/GridPullSpin2D.lua
@@ -9,31 +9,19 @@
 --]]
 
 
-
--------------- globals/initialization --------------
-
-math.randomseed(23)
-target_point = Vector3d(0, 0, 0) -- In global coordinates! Robots have to convert this in their local coordinate system.
-
-PULL = 0
-SPIN = 1
-last_phase = PULL
-
-
-
 -------------- some helper methods --------------
 
 --[[
 ---- Computes the l1 norm of the given vector
 --]]
 function l1norm(input_vector)
-	return math.abs(input_vector.x) + math.abs(input_vector).y
+	return math.abs(input_vector.x) + math.abs(input_vector.y)
 end
 
 --[[
 ---- Computes the l1 distance of the given vectors
 --]]
-function l2distance(v1, v2)
+function l1distance(v1, v2)
 	return l1norm(v1-v2)
 end
 
@@ -53,36 +41,6 @@ end
 
 
 --[[
----- Flips a fair coin and returns either 0 or 1, depending on the result of the coin flip
---]]
-function flip_coin()
-	return math.random(0,1)
-end
-
-
---[[
----- Returns the quadrant of the global coordinate system in which we lie. The quadrants are numberd counterclockwise
----- starting with 1 for the upper-right quadrant.
---]]
-function quadrant()
-	local global_pos = View.get_robot_coordinate_system_origin(View.get_own_identifier())
-	if (global_pos.x >= 0) then
-		if (global_pos.y >= 0) then
-			return 1
-		else
-			return 4
-		end
-	else
-		if (global_pos.y >= 0) then
-			return 2
-		else
-			return 3
-		end
-	end
-end
-
-
---[[
 ---- Returns the target point in the robot's local coordinate system.
 --]]
 function target_point()
@@ -93,58 +51,99 @@ end
 
 
 --[[
----- Chooses a random move that decreases the l1 distance to the target point by exactly one.
+---- Returns whether the given position is currently occupied by one of the visible robots (or by the robot itself).
 --]]
-function pull()
-	local movement_vector
-	if (flip_coin() == 0) then
-		movement_vector = Vector3d(sign(target_point().x), 0, 0)
-	else
-		movement_vector = Vector3d(0, sign(target_point().y), 0)
+function position_occupied(position)
+	if (position.x == 0 and position.y == 0 and position.z == 0) then
+		return true
 	end
-	View.add_position_request(movement_vector);
+	
+	local visible_robots = View.get_visible_robots()
+	for ignore, robot in pairs(visible_robots) do
+		local robot_position = View.get_position(robot)
+		if (robot_position.x == position.x and robot_position.y == position.y and robot_position.z == position.z) then
+			return true
+		end
+	end
+	return false
 end
 
 --[[
----- Chooses a random move that does not change the l1 distance to the target point.
+---- Returns true if there is a robot standing at robot_position and this robot will perform a pull towards position.
 --]]
-function spin()
-	local movement_vector
-	
-	local sign_x = sign(target_point().x)
-	local sign_y = sign(target_point().y)
-	
-	if (sign_x == 0 and sign_y ~= 0) then
-		while (sign_x == 0 and sign_y ~= 0) do
-			sign_x = math.random(-1,1)
-		end
-		movement_vector = Vector3d(sign_x, sign_y, 0)
-	elseif (sign_x ~= 0 and sign_y == 0) then
-		while (sign_x ~= 0 and sign_y == 0) do
-			sign_y = math.random(-1,1)
-		end
-		movement_vector = Vector3d(sign_x, sign_y, 0)
-	else
-		if (flip_coin() == 0) then
-			movement_vector = Vector3d( sign_x, -sign_y, 0)
-		else
-			movement_vector = Vector3d(-sign_x,  sign_y, 0)
-		end
+function robot_pulling_at_position(robot_position, position)
+	if (not position_occupied(robot_position) or position_occupied(position)) then
+		return false
 	end
-	View.add_position_request(movement_vector);
+	
+	local local_target   = target_point() - robot_position
+	local local_position = position - robot_position
+	
+	if (l1norm(local_position) ~= 1) then
+		return false
+	end
+	
+		
+	if (local_target.x == 0) then
+		return sign(local_target.y) == local_position.y
+	end
+	
+	if (local_target.y == 0) then
+		return sign(local_target.x) == local_position.x
+	end
+	
+	
+	if (sign(local_target.x) == local_position.x) then
+		return true
+	elseif (sign(local_target.y) == local_position.y and
+		    position_occupied(robot_position + Vector3d(sign(local_target.x), 0, 0))) then
+		return true
+	else
+		return false
+	end
 end
-
 
 
 -------------- main program --------------
 function main()
-	if (get_robot_last_request_successful(get_own_identifier()) == 0 and last_phase == PULL) then
-		add_color_change_request(1)
-		spin()
-		last_phase = SPIN
-	else
-		add_color_change_request(0)
-		pull()
-		last_phase = PULL
+	local movement_vector = nil
+	local target_point = target_point()
+	
+	-- compute possible pull movements
+	local pull1 = Vector3d(sign(target_point.x), 0, 0)
+	local pull2 = Vector3d(0, sign(target_point.y), 0)
+	
+	-- compute possible spin movements
+	local spin = Vector3d(0,0,0)
+	if (target_point.x < 0 and target_point.y <= 0) then
+		spin = Vector3d(-1, 1, 0)
+	elseif (target_point.x >= 0 and target_point.y < 0) then
+		spin = Vector3d(-1, -1, 0)
+	elseif (target_point.x > 0 and target_point.y >= 0) then
+		spin = Vector3d(1, -1, 0)
+	elseif (target_point.x <= 0 and target_point.y > 0) then
+		spin = Vector3d(1, 1, 0)
+	end
+	
+	-- now check whether we can pull along x, y or spin (in that order!)
+	if (not position_occupied(pull1)) then
+		movement_vector = pull1
+	elseif (not position_occupied(pull2)) then
+		movement_vector = pull2
+	elseif (not position_occupied(spin)) then
+		-- spin is a bit more complicate, because we manually have to check whether another robot is trying a pull on
+		-- our target position (in this case, we resign)
+		local higher_priority_candidates = { Vector3d(1,0,0), Vector3d(0,1,0), Vector3d(-1,0,0), Vector3d(0,-1,0) }
+		
+		if (not robot_pulling_at_position(Vector3d(1,0,0), spin) and
+			not robot_pulling_at_position(Vector3d(0,1,0), spin) and
+			not robot_pulling_at_position(Vector3d(-1,0,0), spin) and
+			not robot_pulling_at_position(Vector3d(0,-1,0), spin)) then
+			movement_vector = spin
+		end
+	end
+	
+	if (movement_vector ~= nil) then
+		View.add_position_request(movement_vector)
 	end
 end
