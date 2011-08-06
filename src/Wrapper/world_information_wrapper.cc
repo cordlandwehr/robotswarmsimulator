49621695f208296ab8bab39fd468584a52a5c762
Michael Knopf <michael@Michael-Desktop.(none)>
2011-08-06 16:12:55 +0200

    Added support for MarkerInformation objects to the WorldInformation wrapper.
    The wrapper class now provides the Lua scripts with the ability to request MarkerInformation objects for a given id. The code still needs checks (i.e. whether an id exists at all, there are corresponding TODO comments). However, there seems to be a bigger problem: Parts of the existing bindings, on which my changes are built upon, do not work.

--- src/Wrapper/world_information_wrapper.cc
+++ src/Wrapper/world_information_wrapper.cc
@@ -11,6 +11,7 @@
 #include "../Model/world_object.h"
 #include "../Model/robot_data.h"
 #include "../Model/robot_identifier.h"
+#include "../Model/marker_identifier.h"
 
 #include <boost/foreach.hpp>
 
@@ -23,8 +24,8 @@ WorldInformationWrapper::WorldInformationWrapper ()
 
 // initialization of static members
 boost::shared_ptr<WorldInformation> WorldInformationWrapper::world_information_;
-std::map< size_t, boost::shared_ptr<Identifier> > WorldInformationWrapper::marker_identifiers_; 
-std::map< size_t, boost::shared_ptr<Identifier> > WorldInformationWrapper::robot_identifiers_; 
+std::map< std::size_t, boost::shared_ptr<MarkerIdentifier> > WorldInformationWrapper::marker_identifiers_; 
+std::map< std::size_t, boost::shared_ptr<RobotIdentifier> > WorldInformationWrapper::robot_identifiers_; 
 
 WorldInformationWrapper::~WorldInformationWrapper ()
 {
@@ -41,28 +42,45 @@ WorldInformationWrapper::set_world_information (const boost::shared_ptr <
   world_information_ = world_information;
 }
 
-const std::vector < size_t >
+const MarkerInformationWrapper
+WorldInformationWrapper::get_marker_information(std::size_t id) {
+  // TODO: Map access should be checked! And pointer handling (looks wrong) ...
+  WorldObject &marker = world_information_->get_according_marker(*(marker_identifiers_[id].get()));
+  MarkerInformationWrapper marker_information(marker.marker_information());
+  return marker_information;
+}
+
+const std::vector < std::size_t >
 WorldInformationWrapper::get_markers () {
-  std::vector<size_t> result;
+  std::vector<std::size_t> result;
   
   BOOST_FOREACH (boost::shared_ptr<WorldObject> marker, world_information_->markers()) {
     // the real id is visible since WorldInformationWrapper is a friend class of Identifier
-    size_t id = marker->id()->id();
-    marker_identifiers_[id] = marker->id();
+    std::size_t id = marker->id()->id();
+    marker_identifiers_[id] = boost::dynamic_pointer_cast<MarkerIdentifier>(marker->id());
     result.push_back(id);
   }
   
   return result;
 }
 
-const std::vector < size_t > 
+const MarkerInformationWrapper
+WorldInformationWrapper::get_robot_information(std::size_t id) {
+  // TODO: Map access should be checked!
+  RobotData &robot = world_information_->get_according_robot_data(robot_identifiers_[id]);
+  MarkerInformationWrapper marker_information(robot.marker_information());
+  return marker_information;
+}
+
+const std::vector < std::size_t > 
 WorldInformationWrapper::get_robots () {
-  std::vector<size_t> result;
+  std::vector<std::size_t> result;
   
   BOOST_FOREACH (boost::shared_ptr<RobotData> robot, world_information_->robot_data()) {
     // the real id is visible since WorldInformationWrapper is a friend class of Identifier
-    size_t id = robot->id()->id();
-    robot_identifiers_[id] = robot->id();
+    std::size_t id = robot->id()->id();
+    // TODO: Handle possible exceptions!
+    robot_identifiers_[id] = boost::dynamic_pointer_cast<RobotIdentifier>(robot->id());
     result.push_back(id);
   }
   
--- src/Wrapper/world_information_wrapper.h
+++ src/Wrapper/world_information_wrapper.h
@@ -14,6 +14,7 @@
 #include <boost/shared_ptr.hpp>
 
 #include "../Model/world_information.h"
+#include "../Wrapper/marker_information_wrapper.h"
 
 namespace LuaWrapper {
  
@@ -27,14 +28,16 @@ public:
   static void set_world_information(const boost::shared_ptr<WorldInformation> &world_information);
   
   // actual wrapper methods
-  static const std::vector<size_t> get_markers();
-  static const std::vector<size_t> get_robots();
+  static const MarkerInformationWrapper get_marker_information(std::size_t id);
+  static const std::vector<std::size_t> get_markers();
+  static const MarkerInformationWrapper get_robot_information(std::size_t id);
+  static const std::vector<std::size_t> get_robots();
   static int get_time();
   
 private:
   static boost::shared_ptr<WorldInformation> world_information_;
-  static std::map< size_t, boost::shared_ptr<Identifier> > marker_identifiers_;
-  static std::map< size_t, boost::shared_ptr<Identifier> > robot_identifiers_;
+  static std::map< std::size_t, boost::shared_ptr<MarkerIdentifier> > marker_identifiers_;
+  static std::map< std::size_t, boost::shared_ptr<RobotIdentifier> > robot_identifiers_;
 };
   
 }
