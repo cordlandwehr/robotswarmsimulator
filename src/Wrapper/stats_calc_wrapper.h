95223723f1355faae5d2205e472b44ac2cc64bdf
Michael Knopf <michael@Michael-Desktop.(none)>
2011-08-23 15:02:13 +0200

    Updates StatsCalc Lua Wrapper.
    The StatsClac Lua Wrapper now includes the StatsCalc::calculate_hop_distance(...) method.

--- src/Wrapper/stats_calc_wrapper.h
+++ src/Wrapper/stats_calc_wrapper.h
@@ -28,6 +28,16 @@ public:
   static int calculate_degree(const std::vector<std::size_t>& ids);
   
   /**
+   * Calculates hop distance between a pair of robots.
+   * 
+   * \param start	Start point (robot).
+   * \param target	End point (robot).
+   * \param ignore	List of edges to Ignore.
+   * \returns		Hop distance.
+   */
+  static int calculate_hop_distance(std::size_t start, std::size_t target, std::vector<std::size_t> ignore);
+  
+  /**
    * Calculates maximal defect.
    * 
    * \returns 		Maximal defect.
