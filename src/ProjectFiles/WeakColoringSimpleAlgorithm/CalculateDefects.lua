94fbf46f4a0c3b85c16eec951914cd9613ecf79d
xynodes <xynodes@gmail.com>
2011-08-28 18:04:36 +0200

    debugging of weak coloring algorithm

---
+++ src/ProjectFiles/WeakColoringSimpleAlgorithm/CalculateDefects.lua
@@ -0,0 +1,5 @@
+
+function main()
+	Statistics.calculate_maximal_defect()
+	Statistics.calculate_total_defects()
+end
--- src/ProjectFiles/WeakColoringSimpleAlgorithm/random_adversary.lua
+++ src/ProjectFiles/WeakColoringSimpleAlgorithm/random_adversary.lua
@@ -4,7 +4,7 @@ function main()
 	me = View.get_own_id()
 	my_marker = View.get_robot_information(me)	
 
-	my_marker:add_data("color", math.random(0, 2))		
+	my_marker:add_data("color", 5)		
 
 	View.add_marker_request(my_marker)
 end
