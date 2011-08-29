28192b5c8ecfbdfee4425cb23a2d3e86855956a2
xynodes <xynodes@gmail.com>
2011-08-29 11:49:28 +0200

    update of weak coloring simple algorithm

--- src/ProjectFiles/WeakColoringSimpleAlgorithm/random_adversary.lua
+++ src/ProjectFiles/WeakColoringSimpleAlgorithm/random_adversary.lua
@@ -2,9 +2,13 @@
 
 function main()
 	me = View.get_own_id()
-	my_marker = View.get_robot_information(me)	
+	my_marker = View.get_robot_information(me)
 
-	my_marker:add_data("color", 5)		
+	color = math.random(0,2)
+
+	my_marker:add_data("color", color)
+
+	log("Adversary " .. me .. " set its color to " .. color);		
 
 	View.add_marker_request(my_marker)
 end
