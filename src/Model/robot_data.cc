a8117f7a5ea311fabca262a50aacca7406c8179b
sirami <sirami@mail.upb.de>
2011-09-09 13:44:17 +0200

    Fixed warning about local variable returned in robot_data

--- src/Model/robot_data.cc
+++ src/Model/robot_data.cc
@@ -149,7 +149,7 @@ boost::shared_ptr<WorldObject> RobotData::clone() const {
 	return boost::shared_ptr<WorldObject>(new RobotData(*this));
 }
 
-const std::vector<boost::shared_ptr<EdgeIdentifier> >& RobotData::get_outgoing_edges() const{
+const std::vector<boost::shared_ptr<EdgeIdentifier> > RobotData::get_outgoing_edges() const{
 	assert(view_.expired() == false);
 	std::vector<boost::shared_ptr<EdgeIdentifier> > result = view_.lock()->get_visible_edges(this->robot());
 	return result;
--- src/Model/robot_data.h
+++ src/Model/robot_data.h
@@ -273,7 +273,7 @@ public:
 	 * Returns constant vector of outgoing edges.
 	 * Outgoing edges are all undirected edges and directed edges with this robot as source.
 	 */
-	const std::vector<boost::shared_ptr<EdgeIdentifier> >& get_outgoing_edges() const;
+	const std::vector<boost::shared_ptr<EdgeIdentifier> > get_outgoing_edges() const;
 	const boost::graph_traits<BoostGraph>::vertex_descriptor& get_boost_vertex_descriptor() const{
 		return vertex_descriptor_;
 	}
