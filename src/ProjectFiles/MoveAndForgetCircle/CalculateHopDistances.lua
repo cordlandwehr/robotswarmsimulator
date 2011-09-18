9c3f5a26fa5ba825d1dd37d52031cfc68cbeacd0
sirami <sirami@mail.upb.de>
2011-09-18 12:04:37 +0200

    move and forget stats stores values in file

--- src/ProjectFiles/MoveAndForgetCircle/CalculateHopDistances.lua
+++ src/ProjectFiles/MoveAndForgetCircle/CalculateHopDistances.lua
@@ -1,4 +1,15 @@
+hopdistances_overall = {}
+
 function main()
+  if WorldInformation.get_time() == 0 then
+    log("info", "Setting up output file for statistics")
+    io.output("maf_circle_stats.csv")
+    robots = WorldInformation.get_robots()
+    for i = 1, #robots/2 do
+      hopdistances_overall[i] = 0
+    end
+  end
+
   if WorldInformation.get_time() % 8 == 0 then
     log("info", "Skipping statistics in round " .. WorldInformation.get_time())
     return
@@ -17,25 +28,39 @@ function main()
     end
   end
   
+  if #long_range_links == 0 then
+    log("info", "no long range links found, exiting statistics")
+    return
+  end
+  
   --for key, value in pairs(bla) do end
   --for index, values in ipairs(bla) do end
   
   -- calculate hop distance of long range links
   log("debug", "Calculating hopdistances of " .. #long_range_links .. " long range links")
-  hopdistances = {}
+  hopdistances_round = {}
+  robots = WorldInformation.get_robots()
+  for i = 1, #robots/2 do
+    hopdistances_round[i] = 0
+  end
   for i = 1, #long_range_links do
-    log("debug", "Getting head and tail of link " .. long_range_links[i])
+    -- get head and tail of link
     head = WorldInformation.get_head(long_range_links[i])
     tail = WorldInformation.get_tail(long_range_links[i])
-    log("debug", "Calculating hopdistance from " .. tail .. " to " .. head)
+    -- calculate distance without long range links
     dist = Statistics.calculate_hop_distance(tail, head, long_range_links)
-    table.insert(hopdistances, dist)
-    log("debug", "Calculated hopdistance from " .. tail .. " to " .. head .. " as " .. dist)
+    -- store distance
+    hopdistances_round[dist] = hopdistances_round[dist] + 1
+    hopdistances_overall[dist] = hopdistances_overall[dist] + 1
   end
   
-  -- print hopdistances
-  log("info", "calculated " .. #hopdistances .. " hopdistances:")
-  for i = 1, #hopdistances do
-    log("info", "distance[" .. i .. "]=" .. hopdistances[i])
+  -- store new hopdistances_overall in file
+  log("debug", "Saving hopdistances...")
+  for i = 1, #hopdistances_overall do
+    if i < #hopdistances_overall then
+      io.write("" .. hopdistances_overall[i] .. ",")
+    else 
+      io.write("" .. hopdistances_overall[i] .. "\n")
+    end
   end
 end
