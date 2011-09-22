8a9ac42754af46b00dc28af2dc5ccbf9603ebc9c
meisteryeti <myeti@mail.upb.de>
2011-09-22 13:59:06 +0200

    updated weak coloring project

--- src/ProjectFiles/WeakColoringFirstIdea/PolynomGenerator.lua
+++ src/ProjectFiles/WeakColoringFirstIdea/PolynomGenerator.lua
@@ -1,3 +1,6 @@
+
+local d = 1
+
 function getNumberOfColors()
 	local number_of_colors = 0;
 	
@@ -21,21 +24,77 @@ function getNumberOfColors()
   return number_of_colors
 end
 
+function table.equals_table(t1, t2)
+	if #t1 ~= #t2 then
+		return false
+	end
+	for k, v in ipairs(t1) do
+		if v ~= t2[k] then
+			return false
+		end
+	end
+	return true
+end
+
+function table.contains_table(t, value)
+	for _, v in ipairs(t) do
+		if table.equals_table(v, value) then
+			return true
+		end
+	end
+	return false
+end
+
+
+function generate_polynomials(degree, modulo, count)
+	local polynomials = {}
+	while #polynomials < count do
+		local poly = {}
+		for j = 1, degree do 
+			local c = math.random(0, modulo-1)
+			table.insert(poly, c)
+		end
+		if not table.contains_table(polynomials, poly) then
+			table.insert(polynomials, poly)
+		end
+	end
+	return polynomials
+end
+
 function main()
 
-	log("Number of colors: " .. getNumberOfColors())
-
-	local d = Statistics.evaluate_polynomial({1,2,3,4,5},5)
-	log("Evaluate Polynomial: " .. d)
+	local M = getNumberOfColors()
+	log("Number of colors: " .. M)
 	
-	local primes = Statistics.generate_primes(10000, 20000);
-	local pstring = "{ "
-	for i = 1 , #primes do
-		pstring = pstring .. primes[i] .. ", "
+	local Delta = Statistics.calculate_degree()
+	local Upsilon = Delta-d
+	
+	if M==0 or Upsilon <= 1 then
+		return
 	end
-	pstring = pstring .. " }"
-	log("Primes: " .. pstring)
+	
+	log("M=" .. M .. ", Delta=" .. Delta .. ", Upsilon=" .. Upsilon  )
+	local kappa = math.ceil(math.log(M)/math.log(Upsilon))
+	local q_min = math.floor(kappa*Upsilon+1)
+	log("kappa=" .. kappa .. ", qmin=" .. q_min )
+	local q = Statistics.generate_primes(q_min, 2*q_min)[1]
+	log("q=" .. q)
 
+	local polynomials = generate_polynomials(kappa, q, M)
+	local pstrings = {}
+	for i = 1, #polynomials do
+		local poly = table.concat(polynomials[i], ",")
+		table.insert(pstrings, poly)
+	end
+
+
+	local nodes = WorldInformation.get_robots()
+	for i = 1 , #nodes do
+		marker = WorldInformation.get_robot_information(nodes[i])
+		marker:add_data("num_colors", M)
+		marker:add_data("polynomials", table.concat(pstrings, ";"))
+		WorldInformation.set_robot_information(nodes[i], marker)
+	end
 end
 
 
