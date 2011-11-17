34800543e7747035b0910545c65ef5b0c5f8d369
Alexander <alexander@desktop.(none)>
2011-11-17 23:03:13 +0100

    Added more scenarios

---
+++ src/ProjectFiles/AdaptiveNodeMovementTree/adaptive_node_movement_DirectlyToOtherNodeWithOneWayThreshold7_header.lua
@@ -0,0 +1,84 @@
+dofile(WorldInformation.get_project_path() .. "/anm_generics.lua")
+
+threshold = 7
+
+memory = {}
+
+function handle_request(request, heap) 
+  -- IDs
+  local a = request.first
+  local b = request.second
+
+  -- positions
+  local pa = posmap[a]
+  local pb = posmap[b]
+  
+  --log("info", "Request between " .. a .. " and " .. b .. ", positioned at " .. pa .. " and " .. pb .. ", respectively")
+
+  local pna = get_next(pa, pb, heap)
+  local over = false
+
+  --log("info", "pna " .. pna .. ", parent(a): " .. parent(pa, heap) .. ", lc(a): " .. lc(pa, heap) .. ", rc(a): " .. rc(pa, heap) )
+
+  --update local threshold values
+  if pna == parent(pa, heap) then
+    memory[a].p = memory[a].p + 1
+    memory[a].l = 0
+    memory[a].r = 0
+    if memory[a].p >= threshold then
+      over = true  
+    end
+  elseif pna == lc(pa, heap) then
+    memory[a].p = 0
+    memory[a].l = memory[a].l + 1
+    memory[a].r = 0    
+    if memory[a].l >= threshold then
+      over = true
+   end
+  elseif pna == rc(pa, heap) then
+    memory[a].p = 0
+    memory[a].l = 0
+    memory[a].r = memory[a].r + 1    
+    if memory[a].r >= threshold then
+      over = true
+   end
+  end
+
+  -- if threshold exceeded, move node
+  if (over == true) then 
+    -- move a to b
+    while hop_distance(pa, pb, heap) > 1 do
+      -- get neighbor position (heap indices) on path
+      local ppna = get_next(pa, pb, heap)
+      -- get neighbors (robot IDs) on path
+      local na = heap[ppna]
+      -- switch a and neighbor of a
+      switch_robots(a, na, heap)
+  
+      -- update positions for they could have changed
+      pa = posmap[a]
+      pb = posmap[b]  
+    end
+  end
+end
+
+
+function get_weight(distance)
+  -- the larger the return value the more likely the selection of the request
+  if distance > 1 then
+    return 0
+  else
+    return 1/distance
+  end
+end
+
+function localSetup(depth)
+  --memory = {}
+  for i = 1, 2^depth-1 do
+    memory[i] = {}
+    memory[i].p = 0
+    memory[i].l = 0
+    memory[i].r = 0
+  end
+end
+
--- src/ProjectFiles/AdaptiveNodeMovementTree/generate_scenario.lua
+++ src/ProjectFiles/AdaptiveNodeMovementTree/generate_scenario.lua
@@ -2,12 +2,13 @@ name = arg[1]
 depth = arg[2]
 script_out = arg[3]
 onios = arg[4]
+project_file_out = arg[5]
 
 func_header = "\nfunction main()\n"
 func_call = "generic_main(\"" .. name ..  "\", " .. depth .. ", get_weight, handle_request, " .. onios .. ", localSetup" .. ")\n"
 func_footer = "end\n"
 
-
+project_file ="PROJECT_NAME=Adaptive Node Movement for Trees\nROBOT_FILENAME=robots.csv\nEDGE_FILENAME=edges.csv\nASG=SYNCHRONOUS_WM\nWORLD_MODIFIERS=" .. script_out .. "\n"
 
 function generate_script()
   local handler = assert(io.open(script_out, "aw"))
@@ -17,10 +18,17 @@ function generate_script()
   handler:close()
 end
 
+function generate_project_file()
+  local handler = assert(io.open(project_file_out, "w"))
+  handler:write(project_file)
+  handler:close()
+end
+
 if #arg < 4 then
   print("Usage: 'lua " .. arg[0] .. " <strategy name> <tree depth> <script output file> <onios (true/false)>' ...")
 else
   print("Generating '" .. script_out .. "' with depth " .. depth .. " and script " .. script_out .. ".")
   generate_script()
+  generate_project_file()
 end
 
